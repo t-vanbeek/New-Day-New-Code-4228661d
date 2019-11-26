649cb78a5b4e2eaf115abee8d19025ba43e97fe4
Thorvald van Beek <54794887+t-vanbeek@users.noreply.github.com>
2019-11-26 09:36:20 +0100

    Tussentijds opslaan en pushen opdracht

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,4 +1,18 @@
 <?php
+
+/*
+1. INPUT bedrag
+2. Maak een CONSTANT Array met de naam GELDEENHEDEN met daarin de volgende geldeenheden (50, 20, 10, 5, 2, 1)
+3. restbedrag = bedrag
+4. Voor elke geldeenheid in GELDEENHEDEN
+    4a) IF restbedrag >= geldeenheid
+        4aa) aantalKeerGeldEenheidInRestBedrag = bereken hoevaak geldeenheid in restbedrag past
+        4ab) restbedrag = restbedrag % geldeenheid
+        4ac) PRINT X keer X euro
+*/
+
+constant($geldeenheden);
+
 $geldeenheden = array(
     50,
     20,
@@ -7,3 +21,12 @@ $geldeenheden = array(
     2,
     1
 );
+
+echo "Bedrag?" . PHP_EOL;
+$restbedrag = readline() . PHP_EOL;
+
+foreach ($geldeenheden as $GELDEENHEDEN) {
+    if ($restbedrag >= $GELDEENHEDEN) {
+        
+    }
+}
